9261f7f85310be0c356ec4c7bacfebb99df97676
Luiz Henrique <luizhenrique204061@gmail.com>
2025-02-24 02:03:19 -0300

    Primeiro Commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pensamentos</title>
+    <link rel="shortcut icon" href="recursos/imagens/brain.png" type="image/x-icon">
+    <link rel="stylesheet" href="recursos/estilos/geral.css">
+</head>
+<body>
+
+<img src="recursos/imagens/brain.png" alt="Logo de rede de pensamentos">
+
+<h2>#pensamentos</h2>
+<a href="pessoas/acessar.php">Acessar minha conta</a>
+<a href="pessoas/cadastrar.php">Cadastrar conta</a>
+    
+</body>
+</html>
---
+++ pessoas/acessar.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pensamentos</title>
+    <link rel="shortcut icon" href="../recursos/imagens/brain.png" type="image/x-icon">
+    <link rel="stylesheet" href="../recursos/estilos/geral.css">
+</head>
+<body>
+
+<img src="../recursos/imagens/brain.png" alt="Logo de rede de pensamentos">
+
+<h2>#pensamentos</h2>
+
+<h4>Acesse sua conta</h4>
+<form action="">
+    <input type="email" placeholder="E-mail">
+    <br>
+    <input type="password" placeholder="Senha">
+    <br>
+
+    <input type="submit" value="Acessar minha conta">
+</form>
+
+<a href="..">Voltar para o início</a>
+
+<?php
+//Se voltar da forma abaixo vai exibir nome da página com php na URL
+//<a href="../index.php">Voltar para o início</a>
+?>
+    
+</body>
+</html>
